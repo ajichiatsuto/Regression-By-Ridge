c317624f8153cfb4bfda611ff30e743bf6764579
ajichiatsuto <s2410004@jaist.ac.jp>
2024-05-21 15:38:07 +0900

    regression_boston

---
+++ regression_boston.py
@@ -0,0 +1,11 @@
+import mglearn as mg
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import Ridge
+
+X, y = mg.datasets.load_extended_boston()
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+ridge = Ridge().fit(X_train, y_train)
+
+print("Training set score: {:.2f}".format(ridge.score(X_train, y_train)))
+print("Test set score: {:.2f}".format(ridge.score(X_test, y_test)))
